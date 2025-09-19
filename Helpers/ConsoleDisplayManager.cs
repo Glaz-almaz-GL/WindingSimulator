36e3da20259fc2d877f3ffcff7651476e1cd6992
Glaz_almaz <glazalmazgl@gmail.com>
2025-09-19 17:17:33 +0300

    Добавьте файлы проекта.

---
+++ Helpers/ConsoleDisplayManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindingSimulator.Helpers
+{
+    public static class ConsoleDisplayManager
+    {
+        /// <summary>
+        /// Выводит цветное сообщение
+        /// </summary>
+        public static void PrintColoredMessage(string message, ConsoleColor color, bool newLine = false)
+        {
+            Console.ForegroundColor = color;
+
+            if (newLine)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.Write(message);
+            }
+
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Выводит пару ключ-значение с цветами
+        /// </summary>
+        public static void PrintKeyValue(string key, string value, ConsoleColor keyColor, ConsoleColor valueColor)
+        {
+            Console.Write(" ");
+            PrintColoredMessage(key + ": ", keyColor);
+            PrintColoredMessage(value, valueColor, true);
+        }
+
+        /// <summary>
+        /// Выводит заголовок секции
+        /// </summary>
+        public static void PrintSectionHeader(string header)
+        {
+            Console.WriteLine();
+            PrintColoredMessage(header, ConsoleColor.Magenta, true);
+            PrintColoredMessage(new string('═', header.Length), ConsoleColor.DarkMagenta, true);
+            Console.WriteLine();
+        }
+    }
+}
